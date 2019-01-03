c9203aac3689698ee6f1a9b23d2e5ea3dc60d5c1
Matt Jones <matt.mattjones@gmail.com>
2019-01-03 14:35:38 -0500

    user configuration scripts init

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Ensure that root is running the script.
+##
+
+USERS="/config-list.conf"
+
+cat ${USERS} | \
+while read USER GROUP SMBPASS DIR ; do
+   
+   groupadd ${GROUP} 2> /dev/null
+   adduser ${USER} -g ${GROUP}
+
+   (echo $SMBPASS; echo $SMBPASS) | passwd --stdin ${USER} > /dev/null
+   echo Added user ${USER}
+
+   smbpasswd -e ${USER} -w ${SMBPASS} > /dev/null
+   (echo $SMBPASS; echo $SMBPASS) | smbpasswd -as ${USER}
+   echo -e "${USER} = ${USER}" >> /etc/samba/smbusers
+
+   mkdir /data/share/${DIR}
+   chown -R  ${USER}:${GROUP} /data/share/${DIR}
+done
